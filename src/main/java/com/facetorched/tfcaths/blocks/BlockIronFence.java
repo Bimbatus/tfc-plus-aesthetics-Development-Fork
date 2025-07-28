15f12748cd616aef3a4c10bf0f895ac9e851ef88
Bimbatus <60115464+Bimbatus@users.noreply.github.com>
2025-07-28 13:27:26 -0500

    Update BlockIronFence.java

--- src/main/java/com/facetorched/tfcaths/blocks/BlockIronFence.java
+++ src/main/java/com/facetorched/tfcaths/blocks/BlockIronFence.java
@@ -1,1 +1,9 @@
+package net.minecraft.block.Blocks;
 
+public class BlockIronFence extends BlockIronBars{
+	public BlockIronFence () {
+		super();
+		this.setIsWoody(); // has collision!
+		this.setIsDamaging();
+	}
+}
