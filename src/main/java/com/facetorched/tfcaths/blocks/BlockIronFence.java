a32cefb9edcda3a1a87244e79a27fe25f495f0e8
Bimbatus <60115464+Bimbatus@users.noreply.github.com>
2025-07-28 13:32:02 -0500

    Update BlockIronFence.java

--- src/main/java/com/facetorched/tfcaths/blocks/BlockIronFence.java
+++ src/main/java/com/facetorched/tfcaths/blocks/BlockIronFence.java
@@ -1,9 +1,12 @@
 package net.minecraft.block.Blocks;
 
 public class BlockIronFence extends BlockIronBars{
-	public BlockIronFence () {
-		super();
-		this.setIsWoody(); // has collision!
-		this.setIsDamaging();
-	}
-}
+	protected IIcon[] icons;
+	
+	public BlockIronFence() {
+		super(Material.metal);
+		this.setHardness(1F);
+		this.setHarvestLevel("pickaxe", 0);
+		this.setCreativeTab(TFCTabs.TFC_BUILDING_BLOCKS);
+		this.setStepSound(Block.soundTypeMetal);
+  }
