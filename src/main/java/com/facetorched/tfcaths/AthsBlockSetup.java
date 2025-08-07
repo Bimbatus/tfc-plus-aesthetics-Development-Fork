983b51512a12657d99eb40b186648b40a13e770b
Bimbatus <60115464+Bimbatus@users.noreply.github.com>
2025-08-07 15:48:43 -0500

    Update AthsBlockSetup.java

--- src/main/java/com/facetorched/tfcaths/AthsBlockSetup.java
+++ src/main/java/com/facetorched/tfcaths/AthsBlockSetup.java
@@ -1656,7 +1656,7 @@ public class AthsBlockSetup {
 		ladysMantle = plantRegistryHelper(new BlockPlant3d().setName(AthsGlobal.LADYS_MANTLE).addVarys(new EnumVary[] {EnumVary.WINTER, EnumVary.SNOW, EnumVary.AUTUMN, EnumVary.FLOWER}).setNamedPart("Side").setNamedVaryParts(new EnumVary [] {EnumVary.DEFAULT, EnumVary.AUTUMN, EnumVary.FLOWER}, new String[] {"Top","Middle","Bottom"}).setFlowerMonthRange(TFC_Time.JUNE, TFC_Time.JULY).setScale(1.2f));
 		giantLeafCoccoloba = plantRegistryHelper(new BlockPlantTree3d().setName(AthsGlobal.GIANT_LEAF_COCCOLOBA).setPart("Leaf").setPart("Trunk").setIsAxisAligned().setScale(3f));
 		yareta = plantRegistryHelper(new BlockPlant3d().setName(AthsGlobal.YARETA).addVarys(new EnumVary[] {EnumVary.SNOW, EnumVary.FLOWER}).setPart("Cushion").setScale(1f));
-		castillaDeVaca = plantRegistryHelper(new BlockPlant3d().setName(AthsGlobal.CASTILLA_DE_VACA).addVary(EnumVary.SNOW).setPart("Frond").setScale(1.0f));
+		castillaDeVaca = plantRegistryHelper(new BlockPlant3d().setName(AthsGlobal.CASTILLA_DE_VACA).addVary(EnumVary.SNOW).setPart("Frond").setPart("Base").setScale(1.0f));
 		cloudForestBlechnum = plantRegistryHelper(new BlockPlantTree3d().setSapling(new ItemStack(ItemSetup.straw)).setName(AthsGlobal.CLOUD_FOREST_BLECHNUM).setPart("Frond").setPart("Trunk").setPart("Wilted_Frond").setIsAxisAligned().setScale(2f));
 		jamesoniaFern = plantRegistryHelper(new BlockPlantStraw3d().setName(AthsGlobal.JAMESONIA_FERN).setPart("Frond").setScale(1.0f));
 	
