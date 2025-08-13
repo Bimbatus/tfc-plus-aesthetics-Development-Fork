fdb8bc3e4eef6242c3bcd10ae5f9f6468c3c1feb
Bimbatus <60115464+Bimbatus@users.noreply.github.com>
2025-08-13 18:22:23 -0500

    Update AthsBlockSetup.java

--- src/main/java/com/facetorched/tfcaths/AthsBlockSetup.java
+++ src/main/java/com/facetorched/tfcaths/AthsBlockSetup.java
@@ -1469,7 +1469,7 @@ public class AthsBlockSetup {
 		ragwort = plantRegistryHelper(new BlockPlantFlower().setName(AthsGlobal.RAGWORT).addVarys(new EnumVary[] {EnumVary.WINTER, EnumVary.SNOW, EnumVary.FLOWER}).setFlowerMonthRange(TFC_Time.JULY, TFC_Time.OCTOBER).setScale(1f));
 		patagonianRagwort = plantRegistryHelper(new BlockPlantFlower().setName(AthsGlobal.PATAGONIAN_RAGWORT).addVarys(new EnumVary[] {EnumVary.WINTER, EnumVary.SNOW, EnumVary.FLOWER}).setFlowerMonthRange(TFC_Time.JULY, TFC_Time.OCTOBER).setScale(1f));
 		redEscallonia = plantRegistryHelper(new BlockPlantFlower().setName(AthsGlobal.RED_ESCALLONIA).addVarys(new EnumVary[] {EnumVary.WINTER, EnumVary.SNOW, EnumVary.FLOWER}).setFlowerMonthRange(TFC_Time.JULY, TFC_Time.OCTOBER).setIsWoody().setScale(2f));
-		barberry = plantRegistryHelper(new BlockPlantFlower().setExtraNames(AthsGlobal.BARBERRY, "Small","Large").addVarys(new EnumVary[] {EnumVary.FRUIT, EnumVary.SNOW, EnumVary.FLOWER}).setFlowerMonthRange(TFC_Time.MAY, TFC_Time.JUNE).setMonthVaryRange(TFC_Time.SEPTEMBER, TFC_Time.OCTOBER, EnumVary.FRUIT).setIsWoody().setScale(3f));
+		barberry = plantRegistryHelper(new BlockPlantFlower().setExtraNames(AthsGlobal.BARBERRY, "Small","Large").addVarys(new EnumVary[] {EnumVary.FRUIT, EnumVary.AUTUMN,EnumVary.WINTER,EnumVary.SNOW, EnumVary.FLOWER}).setFlowerMonthRange(TFC_Time.MAY, TFC_Time.JUNE).setMonthVaryRange(TFC_Time.SEPTEMBER, TFC_Time.OCTOBER, EnumVary.FRUIT).setIsWoody().setScale(3f));
 		
 		//moss carpet
 		mossCarpetGreen = plantRegistryHelper(new BlockPlantCarpet().setColorRange(.65f, 78, .5f, 128, 0f, 0).setExtraNames(AthsGlobal.MOSS_CARPET_GREEN).addVary(EnumVary.SNOW));
