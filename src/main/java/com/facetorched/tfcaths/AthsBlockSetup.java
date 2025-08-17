0550f9473512467e1d7882e261cafbf1d73bb6f6
Bimbatus <60115464+Bimbatus@users.noreply.github.com>
2025-08-17 16:26:21 -0500

    Update AthsBlockSetup.java

--- src/main/java/com/facetorched/tfcaths/AthsBlockSetup.java
+++ src/main/java/com/facetorched/tfcaths/AthsBlockSetup.java
@@ -882,6 +882,7 @@ public class AthsBlockSetup {
 	public static Block tropicalMistletoe;
 	public static Block cycad2;
 	public static Block mullein;
+	public static Block mattedCaputia;
 
 	public static int plantCrossRenderID;
 	public static int plantCropRenderID;
@@ -1473,8 +1474,9 @@ public class AthsBlockSetup {
 		ragwort = plantRegistryHelper(new BlockPlantFlower().setName(AthsGlobal.RAGWORT).addVarys(new EnumVary[] {EnumVary.WINTER, EnumVary.SNOW, EnumVary.FLOWER}).setFlowerMonthRange(TFC_Time.JULY, TFC_Time.OCTOBER).setScale(1f));
 		patagonianRagwort = plantRegistryHelper(new BlockPlantFlower().setName(AthsGlobal.PATAGONIAN_RAGWORT).addVarys(new EnumVary[] {EnumVary.WINTER, EnumVary.SNOW, EnumVary.FLOWER}).setFlowerMonthRange(TFC_Time.JULY, TFC_Time.OCTOBER).setScale(1f));
 		redEscallonia = plantRegistryHelper(new BlockPlantFlower().setName(AthsGlobal.RED_ESCALLONIA).addVarys(new EnumVary[] {EnumVary.WINTER, EnumVary.SNOW, EnumVary.FLOWER}).setFlowerMonthRange(TFC_Time.JULY, TFC_Time.OCTOBER).setIsWoody().setScale(2f));
-		barberry = plantRegistryHelper(new BlockPlantFlower().setExtraNames(AthsGlobal.BARBERRY, "Small","Large").addVarys(new EnumVary[] {EnumVary.FRUIT, EnumVary.AUTUMN,EnumVary.WINTER,EnumVary.SNOW, EnumVary.FLOWER}).setFlowerMonthRange(TFC_Time.MAY, TFC_Time.JUNE).setMonthVaryRange(TFC_Time.SEPTEMBER, TFC_Time.OCTOBER, EnumVary.FRUIT).setIsWoody().setScale(3f));
+		barberry = plantRegistryHelper(new BlockPlantFlower().setExtraNames(AthsGlobal.BARBERRY, "Small","Large").addVarys(new EnumVary[] {EnumVary.FRUIT, EnumVary.AUTUMN,EnumVary.WINTER,EnumVary.SNOW, EnumVary.FLOWER}).setFlowerMonthRange(TFC_Time.MAY, TFC_Time.JUNE).setIsDamaging().setMonthVaryRange(TFC_Time.SEPTEMBER, TFC_Time.OCTOBER, EnumVary.FRUIT).setIsWoody().setScale(3f));
 		mullein = plantRegistryHelper(new BlockPlantFlower().setNames(AthsGlobal.MULLEIN, new String[] {"First_Year","Second_Year"}).addVarys(new EnumVary[] {EnumVary.AUTUMN,EnumVary.WINTER,EnumVary.SNOW, EnumVary.FLOWER}).setFlowerMonthRange(TFC_Time.JUNE, TFC_Time.OCTOBER).setScale(2f));
+		mattedCaputia = plantRegistryHelper(new BlockPlantFlower().setName(AthsGlobal.MATTED_CAPUTIA).addVary(EnumVary.FLOWER}).setFlowerMonthRange(TFC_Time.MARCH, TFC_Time.MAY).setScale(1f));
 		
 		//moss carpet
 		mossCarpetGreen = plantRegistryHelper(new BlockPlantCarpet().setColorRange(.65f, 78, .5f, 128, 0f, 0).setExtraNames(AthsGlobal.MOSS_CARPET_GREEN).addVary(EnumVary.SNOW));
